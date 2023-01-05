f341ed795ba43af2313e4737d889e54addb2e162
HunterX-7 <fraquin7@gmail.com>
2023-01-05 15:39:00 -0500

    Add the logic for the refresh button

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 import './style.css';
-import Score from './modules/score.js';
-import addScore from './modules/addScore.js';
-import listScore from './modules/interface.js';
+// import addScore from './modules/addScore.js';
+// import listScore from './modules/interface.js';
+// import Score from './modules/score.js';
 
 // Elements
 
 const list = document.getElementById('score-list');
 const form = document.getElementById('form');
-
-// Array of scores
-
-const scores = [];
-
-// interface
-
-listScore(scores, list);
+const refresh = document.getElementById('refresh');
 
 // form event
 
@@ -22,9 +15,23 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   const name = document.getElementById('name').value;
   const score = document.getElementById('score').value;
-  const newScore = new Score(name, score);
-  addScore(scores, newScore);
+  // postData
   document.getElementById('name').value = '';
   document.getElementById('score').value = '';
-  listScore(scores, list);
 });
+
+// refresh event
+
+refresh.addEventListener('click', async () => {
+  // getData
+  const row = document.createElement('tr');
+  scores.forEach((score) => {
+    row.innerHTML = `
+            <td>${score.name}</td>
+            <td>${score.score}</td>
+            <td></td>
+            <td></td>  
+        `;
+    list.appendChild(row);
+  });
+})
