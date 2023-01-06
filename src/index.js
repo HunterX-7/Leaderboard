3fb523be19ce3d0b997a6b9162195d848c7ec642
HunterX-7 <fraquin7@gmail.com>
2023-01-05 19:07:00 -0500

    Add the logic to get the data and create a function to display the data with the refresh button

--- src/index.js
+++ src/index.js
@@ -3,12 +3,31 @@ import './style.css';
 // import listScore from './modules/interface.js';
 // import Score from './modules/score.js';
 import postData from './modules/postData.js';
+import getData from './modules/getData.js';
 
 // Elements
 
 const list = document.getElementById('score-list');
 const form = document.getElementById('form');
 const refresh = document.getElementById('refresh');
+
+// display list
+
+const displayData = async () => {
+  const scores = await getData();
+  let scoreRow = '';
+  scores.forEach((score) => {
+    scoreRow += `
+        <tr>
+            <td>${score.user}</td>
+            <td>${score.score}</td>
+            <td></td>
+            <td></td>  
+        </tr>
+        `;
+    list.innerHTML = scoreRow;
+  });
+};
 
 // form event
 
@@ -24,15 +43,5 @@ form.addEventListener('submit', (event) => {
 // refresh event
 
 refresh.addEventListener('click', async () => {
-  // const scores = await getData();
-  const row = document.createElement('tr');
-  scores.forEach((score) => {
-    row.innerHTML = `
-            <td>${score.user}</td>
-            <td>${score.score}</td>
-            <td></td>
-            <td></td>  
-        `;
-    list.appendChild(row);
-  });
-})
+  displayData();
+});
--- src/modules/postData.js
+++ src/modules/postData.js
@@ -1,18 +1,18 @@
 // id vWsxxgUetEjhxUMototD
 
-const postData = async(user, score) => {
-    const resp = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/vWsxxgUetEjhxUMototD/scores', {
-        method: 'POST',
-        body: JSON.stringify({
-            user,
-            score
-          }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        }
-    });
-    const list = await resp.json();
-    return list.results;
-}
+const postData = async (user, score) => {
+  const resp = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/vWsxxgUetEjhxUMototD/scores', {
+    method: 'POST',
+    body: JSON.stringify({
+      user,
+      score,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const list = await resp.json();
+  return list.results;
+};
 
 export default postData;
